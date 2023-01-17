05705f6c65386496657aba84ee9ee7ea43714866
Paulo <paulohenriquesms10@gmail.com>
2023-01-16 21:03:39 -0300

    add: setup prisma

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'Hi'
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Estudar'
+            }
+        }
+    })
+
+    return habits
 })
 
 app.listen({
     port: 3333,
+}).then(() => {
+    console.log('HTTP Server running')
 })
