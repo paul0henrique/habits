a269dc606d9928fa988d4be2b9ca458c6b838e00
Paulo <paulohenriquesms10@gmail.com>
2023-01-19 14:17:17 -0300

    feat: add GET route

--- src/routes.ts
+++ src/routes.ts
@@ -33,6 +33,32 @@ export async function appRoutes(app: FastifyInstance){
         })
 
     app.get('/day', async (request) => {
-        const getDayParams = z.object
+        const getDayParams = z.object({
+          date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+          where: {
+            created_at: {
+              lte: date,
+            },
+            weekDays: {
+              some: {
+                week_day: weekDay,
+              }
+            }
+          }
+        })
+
+        return {
+          possibleHabits,
+        }
+
     })
 }
